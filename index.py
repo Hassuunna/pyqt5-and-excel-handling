a68b750ed841d60f98276586f3264408da82d95d
hassuunna <hassanraafat55@gmail.com>
2022-11-01 22:48:01 +0200

    updated functionality 4

--- index.py
+++ index.py
@@ -1,12 +1,15 @@
 import sys
 from os import path
+from unicodedata import name
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUiType
+from numpy import Infinity
 import pandas as pd
 
 formClass, baseClass = loadUiType(path.join(path.dirname(__file__),'design.ui'))
 
 class MainApp(formClass, baseClass):
+    df = None
     def __init__(self, parent=None):
         super(MainApp, self).__init__(parent)
         QMainWindow.__init__(self)
@@ -16,65 +19,88 @@ class MainApp(formClass, baseClass):
         self.Save_BTN.clicked.connect(self.Save)
 
     def addItemsToTable(self):
-        fileName = QFileDialog.getOpenFileName(self, 'OpenFile')[0]
-        df = pd.read_excel(fileName)
-        if df.size == 0:
+        myFileExcel = QFileDialog.getOpenFileName(self, '', '',"Excel files (*.xlsx)")[0]
+        if myFileExcel == '':
+            QMessageBox.about(self, "Error", "Please Choose a file")
             return
-        df.fillna('', inplace=True)
-        self.tableWidget.setRowCount(df.shape[0])
-        self.tableWidget.setColumnCount(df.shape[1])
-        self.tableWidget.setHorizontalHeaderLabels(df.columns)
-        # returns pandas array object
-        for row in df.iterrows():
+        self.df = pd.read_excel(myFileExcel)
+        if self.df.size == 0:
+            QMessageBox.about(self, "Warning", "file is empty")
+            return
+        self.df.fillna('', inplace=True)
+        self.tableWidget.setRowCount(self.df.shape[0])
+        self.tableWidget.setColumnCount(self.df.shape[1])
+        self.tableWidget.setHorizontalHeaderLabels(self.df.columns)
+
+        for row in self.df.iterrows():
+            row_index = row[0]
             values = row[1]
-            for col_index, value in enumerate(values):
-                if isinstance(value, (float, int)):
-                    value = '{0:0,.0f}'.format(value)
-                tableItem = QTableWidgetItem(str(value))
-                self.tableWidget.setItem(row[0], col_index, tableItem)
+            for col_index, myValue in enumerate(values):
+                if isinstance(myValue, (float, int)):
+                    myValue = '{0:.0f}'.format(myValue)
+                mytableItem = QTableWidgetItem(str(myValue))
+                self.tableWidget.setItem(row_index, col_index, mytableItem)
+
 
     def Average(self):
-        myNumberofCurrentRows = self.tableWidget.rowCount()
-        myNumberofCurrentColumns = self.tableWidget.columnCount()
-        mySums = [0] * myNumberofCurrentColumns
+        myCurrentRows = self.df.shape[0]
+        myCurrentColumns = self.df.shape[1]
+        mySums = [0] * myCurrentColumns
+        myMax = [-Infinity] * myCurrentColumns
+        myMin = [Infinity] * myCurrentColumns
         
-        for row in range(self.myAverageRow):
-            for column in range(myNumberofCurrentColumns):
+        for row in range(myCurrentRows):
+            for column in range(myCurrentColumns):
                 myItem = self.tableWidget.item(row, column)
                 if myItem:
-                    myItemText = myItem.text().replace(',','')
-                    mySums[column] += float(myItemText)
-        
-        myAverages = [x / self.myAverageRow for x in mySums]
-        if self.myAverageRow >= myNumberofCurrentRows:
-            self.tableWidget.insertRow(self.myAverageRow)
-        for column in range(myNumberofCurrentColumns):
+                    myItemText = float(myItem.text())
+                    mySums[column] += myItemText
+                    if myItemText > myMax[column]:
+                        myMax[column] = myItemText
+                    if myItemText < myMin[column]:
+                        myMin[column] = myItemText
+
+        myAverages = [x / myCurrentRows for x in mySums]
+
+        #self.addRow(myMin)
+        self.tableWidget.insertRow(myCurrentRows)
+        for column in range(myCurrentColumns):
+            tableItem = QTableWidgetItem(str(myMin[column]))
+            self.tableWidget.setItem(myCurrentRows, column,tableItem)
+
+        self.tableWidget.insertRow(myCurrentRows+1)
+        for column in range(myCurrentColumns):
+            tableItem = QTableWidgetItem(str(myMax[column]))
+            self.tableWidget.setItem(myCurrentRows+1, column,tableItem)
+
+        self.tableWidget.insertRow(myCurrentRows+2)
+        for column in range(myCurrentColumns):
             tableItem = QTableWidgetItem(str(myAverages[column]))
-            self.tableWidget.setItem(self.myAverageRow, column,tableItem)
-    
-    def Save(self):
-        columnHeaders = []
+            self.tableWidget.setItem(myCurrentRows+2, column,tableItem)
 
-        # create column header list
-        for j in range(self.tableWidget.model().columnCount()):
-            columnHeaders.append(self.tableWidget.horizontalHeaderItem(j).text())
+    def addRow(self, listToAdd):
+        #addlist
+        print("row added succesfully!")
 
+
+    def Save (self):
+        columnHeaders = self.df.columns.tolist()
         df = pd.DataFrame(columns=columnHeaders)
 
-        # create dataframe object recordset
         for row in range(self.tableWidget.rowCount()):
             for col in range(self.tableWidget.columnCount()):
                 df.at[row, columnHeaders[col]] = self.tableWidget.item(row, col).text()
 
-        df.to_excel('Dummy.xlsx', index=False)
-        print('Excel file exported')
+        myNewName = QFileDialog.getSaveFileName(self,'Save as', '', 'Excel (*.xlsx)')[0]
+        
+        df.to_excel(myNewName)
 
 
 def main():
-    app = QApplication(sys.argv)
-    window = MainApp()
-    window.show()
-    app.exec_()
+    myApp = QApplication(sys.argv)
+    myWindow = MainApp()
+    myWindow.show()
+    myApp.exec_()
 
 if __name__ == '__main__':
     main()
