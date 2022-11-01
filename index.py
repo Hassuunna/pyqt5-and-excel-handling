ce8286fed15648b49bc7effba9216b6327e74c43
hassuunna <hassanraafat55@gmail.com>
2022-11-01 22:43:09 +0200

    added py file to load design

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import sys
 from os import path
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUiType
+import pandas as pd
 
 formClass, baseClass = loadUiType(path.join(path.dirname(__file__),'design.ui'))
 
@@ -10,6 +11,28 @@ class MainApp(formClass, baseClass):
         super(MainApp, self).__init__(parent)
         QMainWindow.__init__(self)
         self.setupUi(self)
+        self.Browse_BTN.clicked.connect(self.addItemsToTable)
+        self.Calculate_BTN.clicked.connect(self.Average)
+        self.Save_BTN.clicked.connect(self.Save)
+
+    def addItemsToTable(self):
+        fileName = QFileDialog.getOpenFileName(self, 'OpenFile')[0]
+        df = pd.read_excel(fileName)
+        if df.size == 0:
+            return
+        df.fillna('', inplace=True)
+        self.tableWidget.setRowCount(df.shape[0])
+        self.tableWidget.setColumnCount(df.shape[1])
+        self.tableWidget.setHorizontalHeaderLabels(df.columns)
+        # returns pandas array object
+        for row in df.iterrows():
+            values = row[1]
+            for col_index, value in enumerate(values):
+                if isinstance(value, (float, int)):
+                    value = '{0:0,.0f}'.format(value)
+                tableItem = QTableWidgetItem(str(value))
+                self.tableWidget.setItem(row[0], col_index, tableItem)
+
 
 def main():
     app = QApplication(sys.argv)
