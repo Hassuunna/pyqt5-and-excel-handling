3d5239c8dc16b0f998374c415d84d9f208bde406
hassuunna <hassanraafat55@gmail.com>
2022-11-01 23:00:44 +0200

    removed comments to prepare build

--- index.py
+++ index.py
@@ -9,7 +9,6 @@ import pandas as pd
 import pandasModel
 
 sourcePath = path.join(path.dirname(__file__),'design3.ui')
-
 formClass, baseClass = loadUiType(sourcePath)
 
 class MainApp(formClass, baseClass):
@@ -40,7 +39,6 @@ class MainApp(formClass, baseClass):
         self.tableWidget.setRowCount(self.df.shape[0])
         self.tableWidget.setColumnCount(self.df.shape[1])
         self.tableWidget.setHorizontalHeaderLabels(self.df.columns)
-
         for row in self.df.iterrows():
             row_index = row[0]
             values = row[1]
@@ -52,10 +50,8 @@ class MainApp(formClass, baseClass):
 
 
     def Calculate(self):
-        #make this dynamic done
         first_header = self.df.columns.tolist()[0]
         mod_df = self.df.set_index(first_header)
-        #what if startValue and endValue are empty? done
         self.start = int(self.startValue.text()) if not self.startValue.text() == '' else int(mod_df.index[0])
         self.end = int(self.endValue.text()) if not self.endValue.text() == '' else int(mod_df.index[-1])
         if self.end < self.start: 
@@ -64,56 +60,30 @@ class MainApp(formClass, baseClass):
             self.start = temp
         interval = mod_df.loc[self.start: self.end]
         print(interval)
-        # round done
         self.result_df = interval.aggregate(['min', 'max', 'mean']).astype(int)
         model = pandasModel(self.result_df)
         self.tableView.setModel(model)
 
 
     def Append(self):
-        # turn interval into string
         self.intervals.append(str(self.start) + " - " + str(self.end))
-        # define new empty row to insert
         new_row = pd.Series({})
-        # concat the old existing data then empty row then new data
         self.final_result = pd.concat([self.final_result, new_row.to_frame().T, self.result_df])
 
 
     def Save (self):
-        #append start and end done
-        #add merged big cell done
-        #don't overwrite done
-        # get file name
         excelFileName = QFileDialog.getSaveFileName(self, 'Save as', '', 'Excel (*.xlsx)')[0]
-        # startcol = 1 to leave space for intervals
         self.final_result.to_excel(excelFileName, startcol=1)
-
-        #with pd.ExcelWriter(myNewName, mode="a", if_sheet_exists='overlay') as writer:
-        # to open the excel sheet and if it has macros
         wb = load_workbook(excelFileName, read_only=False)
-        
-        # get sheetname from the file
         sheetname = wb['Sheet1']
-        
-        #count existing rows in sheet
         begin = sheetname.max_row + 2 - len(self.intervals) * 4
-        
-        # write to row ,col explicitly, this type of writing is useful to
-        # write something in loops
         for interval in self.intervals:
-            #merge 3 cells corresponding to min max mean
             sheetname.merge_cells('A'+str(begin)+':A'+str(begin+2))
-            # remove zero from column B in index5.2
             sheetname['B' + str(begin-1)] = ""
-            # write interval string
             sheetname['A' + str(begin)] = interval
-            # align cell text in center
             cell = sheetname.cell(begin, 1)
             cell.alignment = Alignment(horizontal='center', vertical='center')
-            # increment by 4 to write the next interval
             begin+=4
-        
-        # save it as the original file
         wb.save(excelFileName)
 
 
