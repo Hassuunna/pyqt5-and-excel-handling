7d17dbf18296d3cfa3738430c12d2d1bbfae0d8c
hassuunna <hassanraafat55@gmail.com>
2022-11-01 22:50:15 +0200

    updated functionality 5

--- index.py
+++ index.py
@@ -1,25 +1,50 @@
 import sys
 from os import path
-from unicodedata import name
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 from PyQt5.uic import loadUiType
 from numpy import Infinity
 import pandas as pd
+
+class PandasModel(QAbstractTableModel):
+    def __init__(self, data, parent=None):
+        QAbstractTableModel.__init__(self, parent)
+        self._data = data
+    
+    def rowCount(self, parent=None):
+        return len(self._data.values)
+
+    def columnCount(self, parent=None):
+        return self._data.columns.size
+
+    def data(self, index, role=Qt.DisplayRole):
+        if index.isValid():
+            if role == Qt.DisplayRole:
+                return QVariant(str(
+                    self._data.iloc[index.row()][index.column()]))
+        return QVariant()
+    
+    def headerData(self, rowcol, orientation, role=Qt.DisplayRole):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return self._data.columns[rowcol]
+        if orientation == Qt.Vertical and role == Qt.DisplayRole:
+            return self._data.index[rowcol]
 
 formClass, baseClass = loadUiType(path.join(path.dirname(__file__),'design.ui'))
 
 class MainApp(formClass, baseClass):
     df = None
+    result_df = None
     def __init__(self, parent=None):
         super(MainApp, self).__init__(parent)
         QMainWindow.__init__(self)
         self.setupUi(self)
         self.Browse_BTN.clicked.connect(self.addItemsToTable)
-        self.Calculate_BTN.clicked.connect(self.Average)
+        self.Calculate_BTN.clicked.connect(self.Calculate)
         self.Save_BTN.clicked.connect(self.Save)
 
     def addItemsToTable(self):
-        myFileExcel = QFileDialog.getOpenFileName(self, '', '',"Excel files (*.xlsx)")[0]
+        myFileExcel = QFileDialog.getOpenFileName(self, '', '',"Excel files (*.xlsx *.xls)")[0]
         if myFileExcel == '':
             QMessageBox.about(self, "Error", "Please Choose a file")
             return
@@ -37,63 +62,32 @@ class MainApp(formClass, baseClass):
             values = row[1]
             for col_index, myValue in enumerate(values):
                 if isinstance(myValue, (float, int)):
-                    myValue = '{0:.0f}'.format(myValue)
+                    myValue = int(myValue)
                 mytableItem = QTableWidgetItem(str(myValue))
                 self.tableWidget.setItem(row_index, col_index, mytableItem)
 
 
-    def Average(self):
-        myCurrentRows = self.df.shape[0]
-        myCurrentColumns = self.df.shape[1]
-        mySums = [0] * myCurrentColumns
-        myMax = [-Infinity] * myCurrentColumns
-        myMin = [Infinity] * myCurrentColumns
-        
-        for row in range(myCurrentRows):
-            for column in range(myCurrentColumns):
-                myItem = self.tableWidget.item(row, column)
-                if myItem:
-                    myItemText = float(myItem.text())
-                    mySums[column] += myItemText
-                    if myItemText > myMax[column]:
-                        myMax[column] = myItemText
-                    if myItemText < myMin[column]:
-                        myMin[column] = myItemText
-
-        myAverages = [x / myCurrentRows for x in mySums]
-
-        #self.addRow(myMin)
-        self.tableWidget.insertRow(myCurrentRows)
-        for column in range(myCurrentColumns):
-            tableItem = QTableWidgetItem(str(myMin[column]))
-            self.tableWidget.setItem(myCurrentRows, column,tableItem)
-
-        self.tableWidget.insertRow(myCurrentRows+1)
-        for column in range(myCurrentColumns):
-            tableItem = QTableWidgetItem(str(myMax[column]))
-            self.tableWidget.setItem(myCurrentRows+1, column,tableItem)
-
-        self.tableWidget.insertRow(myCurrentRows+2)
-        for column in range(myCurrentColumns):
-            tableItem = QTableWidgetItem(str(myAverages[column]))
-            self.tableWidget.setItem(myCurrentRows+2, column,tableItem)
-
-    def addRow(self, listToAdd):
-        #addlist
-        print("row added succesfully!")
+    def Calculate(self):
+        #make this dynamic done
+        first_header = self.df.columns.tolist()[0]
+        mod_df = self.df.set_index(first_header)
+        #what if startValue and endValue are empty? done
+        start = int(self.startValue.text()) if not self.startValue.text() == '' else mod_df.index[0]
+        end = int(self.endValue.text()) if not self.endValue.text() == '' else mod_df.index[-1]
+        interval = mod_df.loc[start: end]
+        # round done
+        self.result_df = interval.aggregate(['min', 'max', 'mean']).astype(int)
+        model = PandasModel(self.result_df)
+        self.tableView.setModel(model)
 
 
     def Save (self):
-        columnHeaders = self.df.columns.tolist()
-        df = pd.DataFrame(columns=columnHeaders)
-
-        for row in range(self.tableWidget.rowCount()):
-            for col in range(self.tableWidget.columnCount()):
-                df.at[row, columnHeaders[col]] = self.tableWidget.item(row, col).text()
-
+        #append start and end
+        #add merged big cell
+        #don't overwrite
         myNewName = QFileDialog.getSaveFileName(self,'Save as', '', 'Excel (*.xlsx)')[0]
-        
-        df.to_excel(myNewName)
+        self.result_df.to_excel(myNewName)
+
 
 
 def main():
