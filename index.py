c16342a36418c54b10b7ea0b1030a6f769929327
hassuunna <hassanraafat55@gmail.com>
2022-11-01 22:52:07 +0200

    updated functionality 8

--- index.py
+++ index.py
@@ -3,7 +3,8 @@ from os import path
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.uic import loadUiType
-import openpyxl
+from openpyxl import load_workbook
+from openpyxl.styles import Alignment
 import pandas as pd
 
 class PandasModel(QAbstractTableModel):
@@ -23,20 +24,22 @@ class PandasModel(QAbstractTableModel):
                 return QVariant(str(
                     self._data.iloc[index.row()][index.column()]))
         return QVariant()
-    
+
     def headerData(self, rowcol, orientation, role=Qt.DisplayRole):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
             return self._data.columns[rowcol]
         if orientation == Qt.Vertical and role == Qt.DisplayRole:
             return self._data.index[rowcol]
 
-formClass, baseClass = loadUiType(path.join(path.dirname(__file__),'design3.ui'))
+sourcePath = path.join(path.dirname(__file__),'design3.ui')
+
+formClass, baseClass = loadUiType(sourcePath)
 
 class MainApp(formClass, baseClass):
     start, end = 0, 0
     intervals = []
-    df = None
-    result_df = None
+    df = pd.DataFrame()
+    result_df = pd.DataFrame()
     final_result = pd.DataFrame()
     def __init__(self, parent=None):
         super(MainApp, self).__init__(parent)
@@ -76,9 +79,14 @@ class MainApp(formClass, baseClass):
         first_header = self.df.columns.tolist()[0]
         mod_df = self.df.set_index(first_header)
         #what if startValue and endValue are empty? done
-        self.start = int(self.startValue.text()) if not self.startValue.text() == '' else mod_df.index[0]
-        self.end = int(self.endValue.text()) if not self.endValue.text() == '' else mod_df.index[-1]
+        self.start = int(self.startValue.text()) if not self.startValue.text() == '' else int(mod_df.index[0])
+        self.end = int(self.endValue.text()) if not self.endValue.text() == '' else int(mod_df.index[-1])
+        if self.end < self.start: 
+            temp = self.end
+            self.end = self.start
+            self.start = temp
         interval = mod_df.loc[self.start: self.end]
+        print(interval)
         # round done
         self.result_df = interval.aggregate(['min', 'max', 'mean']).astype(int)
         model = PandasModel(self.result_df)
@@ -86,18 +94,51 @@ class MainApp(formClass, baseClass):
 
 
     def Append(self):
-        self.intrevals.append(str(self.start) + " - " + str(self.end))
+        # turn interval into string
+        self.intervals.append(str(self.start) + " - " + str(self.end))
+        # define new empty row to insert
         new_row = pd.Series({})
+        # concat the old existing data then empty row then new data
         self.final_result = pd.concat([self.final_result, new_row.to_frame().T, self.result_df])
-        print(self.final_result)
 
 
     def Save (self):
-        #append start and end
-        #add merged big cell
-        #don't overwrite
-        myNewName = QFileDialog.getSaveFileName(self, 'Save as', '', 'Excel (*.xlsx)')[0]
-        self.final_result.to_excel(myNewName, startcol=1)
+        #append start and end done
+        #add merged big cell done
+        #don't overwrite done
+        # get file name
+        excelFileName = QFileDialog.getSaveFileName(self, 'Save as', '', 'Excel (*.xlsx)')[0]
+        # startcol = 1 to leave space for intervals
+        self.final_result.to_excel(excelFileName, startcol=1)
+
+        #with pd.ExcelWriter(myNewName, mode="a", if_sheet_exists='overlay') as writer:
+        # to open the excel sheet and if it has macros
+        wb = load_workbook(excelFileName, read_only=False)
+        
+        # get sheetname from the file
+        sheetname = wb['Sheet1']
+        
+        #count existing rows in sheet
+        begin = sheetname.max_row + 2 - len(self.intervals) * 4
+        
+        # write to row ,col explicitly, this type of writing is useful to
+        # write something in loops
+        for interval in self.intervals:
+            #merge 3 cells corresponding to min max mean
+            sheetname.merge_cells('A'+str(begin)+':A'+str(begin+2))
+            # remove zero from column B in index5.2
+            sheetname['B' + str(begin-1)] = ""
+            # write interval string
+            sheetname['A' + str(begin)] = interval
+            # align cell text in center
+            cell = sheetname.cell(begin, 1)
+            cell.alignment = Alignment(horizontal='center', vertical='center')
+            # increment by 4 to write the next interval
+            begin+=4
+        
+        # save it as the original file
+        wb.save(excelFileName)
+
 
 
 def main():
